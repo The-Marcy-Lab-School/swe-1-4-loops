69a15af699516209831107cf9ecec33b952fbf61
Ben Spector <111444562+benspector-mls@users.noreply.github.com>
2024-10-22 13:26:53 -0400

    Update modify.js

--- src/modify.js
+++ src/modify.js
@@ -8,10 +8,9 @@ const whileToFor = () => {
 
 const continueGuardClause = () => {
   for (let i = 1; i < 5; i++) {
-    if (!(i === 2) && !(i === 3)) {
-      console.log("Sure glad this isn't 2 or 3");
-      console.log(i);
-    }
+    // Add a guard clause to skip 2 and 3
+    console.log("Sure glad this isn't 2 or 3");
+    console.log(i);
   }
 };
 
